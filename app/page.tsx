4f80fc0d40a88d31d00aabd6cfddaa4673ffe89b
Piyush Prateek <piyushprateek231996@gmail.com>
2025-02-18 23:29:43 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,42 @@
+import HeroSection from "@/components/hero";
+import { Card, CardContent } from "@/components/ui/card";
+import { features } from "@/data/features";
+
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div>
+      <div className="grid-background">
+      </div>
+      <HeroSection />
+
+      <section>
+        <div>
+          <h2 className="text-3xl font-bold tracking-tighter text-center mb-12">
+            Powerful Features for Your Career Growth
+          </h2>
+          <div>{features.map((feature, index) => {
+            return (
+
+              <Card
+                key={index}
+                className="border-2 hover:border-primary transition-colors duration-300"
+              >
+                <CardContent className="pt-6 text-center flex flex-col items-center">
+                  <div className="flex flex-col items-center justify-center">
+                    {feature.icon}
+                    <h3 className="text-xl font-bold mb-2">{feature.title}</h3>
+                    <p className="text-muted-foreground">
+                      {feature.description}
+                    </p>
+                  </div>
+                </CardContent>
+              </Card>
+            )
+          })}
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
