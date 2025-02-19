34c4d7502d77b702584d473d672f7a299c2d7173
Piyush Prateek <piyushprateek231996@gmail.com>
2025-02-20 01:10:59 +0530

    api - user working

--- app/(main)/onboarding/page.tsx
+++ app/(main)/onboarding/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { redirect } from 'next/navigation'
+import { getUserOnboardingStatus } from '@/actions/user'
 
-const OnboardingPage = () => {
+const OnboardingPage = async () => {
     //Check if user is already onboarded
+    const { isOnboarded } = await getUserOnboardingStatus()
+
+    if (isOnboarded) {
+        redirect("/dashboard")
+    }
     return (
         <main>OnboardingPage</main>
     )
