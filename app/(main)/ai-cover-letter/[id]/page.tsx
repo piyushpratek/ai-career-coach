583e1b9caf90d92600b058fce78da26ba85a412b
Piyush Prateek <piyushprateek231996@gmail.com>
2025-03-12 21:00:39 +0530

    params fix

--- app/(main)/ai-cover-letter/[id]/page.tsx
+++ app/(main)/ai-cover-letter/[id]/page.tsx
@@ -6,27 +6,29 @@ import { Button } from '@/components/ui/button';
 import { ArrowLeft } from 'lucide-react';
 import CoverLetterPreview from '../_components/cover-letter-preview';
 
-interface PageProps {
-    params: {
-        id: string;
-    };
-}
+// interface PageProps {
+//     params: {
+//         id: string;
+//     };
+// }
 
-const EditCoverLetterPage = async ({ params }: PageProps) => {
+const EditCoverLetterPage = async ({ params }: { params: Promise<{ id: string }> }) => {
     // const { id } = params
     const resolvedParams = await params; // Ensure it's awaited
-    console.log("Params Type:", typeof params, params);
+    // console.log("Params Type:", typeof params, params);
 
-    // console.log("Params:", resolvedParams);
+    console.log("resolvedParams:", resolvedParams);
 
     const coverLetter = await getCoverLetter(resolvedParams.id);
 
     return (
+
         <div className="container mx-auto py-6">
             <div className="flex flex-col space-y-2">
                 <Link href="/ai-cover-letter">
                     <Button variant="link" className="gap-2 pl-0">
                         <ArrowLeft className="h-4 w-4" />
+
                         Back to Cover Letters
                     </Button>
                 </Link>
